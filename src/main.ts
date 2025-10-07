bea5e92e4ae1c7da433825220da627c54354ebd1
Dipika Rajesh <dipika.rajesh@gmail.com>
2025-10-07 17:57:35 +0000

    Create and increase counter (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,14 @@ const mainButton = document.createElement("button");
 mainButton.innerHTML = "🍪";
 mainButton.classList.add("icon");
 document.body.append(mainButton);
+
+const counterDiv = document.createElement("div");
+counterDiv.innerHTML = "No clicks yet. Click the Big Cookie to get more.";
+document.body.append(counterDiv);
+
+let counter = 0;
+
+mainButton.addEventListener("click", (_) => {
+  counter += 1;
+  counterDiv.innerHTML = counter.toString();
+});
