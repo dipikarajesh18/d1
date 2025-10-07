23f1bd8ea766356bd966a6917858430984fe78f3
Dipika Rajesh <dipika.rajesh@gmail.com>
2025-10-07 18:42:52 +0000

    Upgrades!!!!! (step 5 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,44 @@
 import "./style.css";
 
+// Create UI elements
+
 const mainButton = document.createElement("button");
 
 mainButton.innerHTML = "🍪";
 mainButton.classList.add("icon");
 document.body.append(mainButton);
 
-let counter = 0; // number of cookies
-const counterIncreaseRate = 1; // cookies per second
+const upgradeButton = document.createElement("button");
 
-function updateCookies(increase: number) {
-  counter += counterIncreaseRate * increase;
-  counterDiv.innerHTML = `${counter.toFixed(1)} cookies`;
-}
+upgradeButton.innerHTML = "Upgrade";
+upgradeButton.disabled = true;
+upgradeButton.classList.add("icon");
+document.body.append(upgradeButton);
 
 const counterDiv = document.createElement("div");
 counterDiv.innerHTML = "No clicks yet. Click the Big Cookie to get more.";
 document.body.append(counterDiv);
 
+// Define main game state
+
+let counter = 0; // number of cookies
+let counterIncreaseRate = 0; // cookies per second
+
+// Define main game logic
+
+function updateCookies(increase: number) {
+  counter += increase;
+  counterDiv.innerHTML = `${counter.toFixed(1)} cookies`;
+
+  // todo: change the disabled stutus of the upgrade button based on if they can afford it or not
+  upgradeButton.disabled = counter < 10;
+}
+
 mainButton.addEventListener("click", (_) => {
   updateCookies(1);
 });
 
-let lastTickTime = 0;
+let lastTickTime = Date.now();
 function tick() {
   const now = Date.now();
   console.log(`old: ${lastTickTime}`);
@@ -33,5 +49,9 @@ function tick() {
   updateCookies(counterIncreaseRate * delta);
   requestAnimationFrame(tick); // temporal recursion
 }
-
 requestAnimationFrame(tick);
+
+upgradeButton.addEventListener("click", (_) => {
+  updateCookies(-10);
+  counterIncreaseRate += 1;
+});
