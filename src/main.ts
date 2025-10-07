1345ed39ea1cf06a207bb42cb80c3d630dbe66c7
Dipika Rajesh <dipika.rajesh@gmail.com>
2025-10-07 17:47:47 +0000

    Add button with cookie emoji (step 1 complete)
    Co-authored-by: Adam M. Smith <adam@adamsmith.as>
    Co-authored-by: Sol Choi <solthe@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const mainButton = document.createElement("button");
+
+mainButton.innerHTML = "🍪";
+mainButton.classList.add("icon");
+document.body.append(mainButton);
